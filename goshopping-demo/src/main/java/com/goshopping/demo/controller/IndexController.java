6f5ec189669b441c229b42a972f95fd963a710cb
yujianzhong <1120384609@qq.com>
2020-12-11 15:50:32 +0800

    整合nacos配置管理

--- goshopping-demo/src/main/java/com/goshopping/demo/controller/IndexController.java
+++ goshopping-demo/src/main/java/com/goshopping/demo/controller/IndexController.java
@@ -2,10 +2,10 @@ package com.goshopping.demo.controller;
 
 import com.goshopping.common.api.ResponseResult;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,12 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Api(tags = "首页模块")
 @RestController
+@RefreshScope
 public class IndexController {
 
-    @ApiImplicitParam(name = "name", value = "姓名", required = true)
+    @Value("${service.name}")
+    private String serverName;
+
     @ApiOperation("向客人问好")
     @GetMapping("/sayHi")
-    public ResponseResult<String> sayHi(@RequestParam(value = "name") String name) {
-        return ResponseResult.success("Hi:" + name);
+    public ResponseResult<String> sayHi() {
+        return ResponseResult.success("Hi:" + serverName);
     }
 }
