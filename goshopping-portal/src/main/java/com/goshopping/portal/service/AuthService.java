21edba23b0d820436b653e971771b1b06d100904
yujianzhong <1120384609@qq.com>
2021-06-04 21:30:58 +0800

    修改portal模块

---
+++ goshopping-portal/src/main/java/com/goshopping/portal/service/AuthService.java
@@ -0,0 +1,20 @@
+package com.goshopping.portal.service;
+
+import com.goshopping.common.api.CommonResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+/**
+ * 认证服务远程调用
+ * Created by macro on 2020/7/19.
+ */
+@FeignClient("mall-auth")
+public interface AuthService {
+
+    @PostMapping(value = "/oauth/token")
+    CommonResult getAccessToken(@RequestParam Map<String, String> parameters);
+
+}
